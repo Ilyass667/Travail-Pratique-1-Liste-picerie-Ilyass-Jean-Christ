332b848b0dd6ad4fb4ae0f87eb7d80dd96e5a30c
jean chrys nadaud <jc@Jc.local>
2024-10-21 09:02:53 -0400

    debut layout page1

--- Liste_Epicerie/app/src/main/java/com/example/liste_epicerie/MainActivity.kt
+++ Liste_Epicerie/app/src/main/java/com/example/liste_epicerie/MainActivity.kt
@@ -1,10 +1,36 @@
 package com.example.liste_epicerie
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+
+data class GenericItem(var name: String, var quantity: Int, var image: Int)
+
+class GenericItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val layout: ConstraintLayout
+    val nomProduit: TextView
+    val img: ImageView
+    val img2: ImageView
+
+
+    init {
+        layout = itemView as ConstraintLayout
+        nomProduit = itemView.findViewById(R.id.nomProduits)
+        img = itemView.findViewById(R.id.barre)
+        img2 = itemView.findViewById(R.id.infos)
+    }
+}
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
